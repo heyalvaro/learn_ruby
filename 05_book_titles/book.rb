8236d753bcd1393d05862abe4174c07ab59ca401
Alvaro Sanchez Diaz <heyalvaro@gmail.com>
2016-11-25 22:21:10 -0600

    Solved exercise 5

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,25 @@
 class Book
-# write your code here
+    attr_accessor :title
+    
+    def title=(my_title)
+        @title = book_capitalize(my_title)
+    end
+
+    def book_capitalize(my_title)
+        forbidden_words = ["and","in","the","of","a","an"]
+        array = my_title.split(' ')
+        new_arr = []
+        length = array.length
+        
+        new_arr.push(array[0].capitalize)
+        
+        (length-1).times do |i|
+            if(forbidden_words.include? array[i+1])
+                new_arr.push(array[i+1])
+            else
+                new_arr.push(array[i+1].capitalize)
+            end
+        end
+        return new_arr.join(' ')        
+    end
 end
